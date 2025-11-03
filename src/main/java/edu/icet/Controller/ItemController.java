34033b6467c73cae1d8a491a9afeb290b945a808
Pradeep Rohan <kumararoha14@gmail.com>
2025-11-03 15:25:00 +0530

    Create Customer Controller API and Item Control API

--- src/main/java/edu/icet/Controller/ItemController.java
+++ src/main/java/edu/icet/Controller/ItemController.java
@@ -4,16 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/customer")
-public class CustomerController {
+@RequestMapping("/item")
+public class ItemController {
     @GetMapping("/name")
     public String get(){
-        return "Rohan";
-    }
-    @GetMapping("/age")
-    public Integer getAge(){
-        return 18;
+        return "item";
     }
 }
